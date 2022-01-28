79f4de361767a6b7de5459606366426efb155a29
Shinichi Okada <okada.shin@gmail.com>
2022-01-28 14:23:44 +0900

    Added modals.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,26 @@
-<ul mx-auto my-auto>
-	<li><a href="svelte-sidebar-single">Svelte-sidebar Sidebar Component Default</a></li>
-	<li><a href="svelte-sidebar-single2">Svelte-sidebar Sidebar Component Styled</a></li>
-	<li>
-		<a href="svelte-sidebar-multiple">Svelte-sidebar Multiple-menu Sidebar Default</a>
-	</li>
-	<li>
-		<a href="svelte-sidebar-multiple2">Svelte-sidebar Multiple-menu Sidebar Default</a>
-	</li>
-</ul>
+<div class="p-8 container mx-auto md:container md:mx-auto">
+	<h1 class="p-1">CodewithShin: SvelteKit Demo</h1>
+	<h2 class="p-1">Sidebar</h2>
+	<ul class="list-inside">
+		<li class="p-1">
+			<a href="svelte-sidebar-single">Svelte-sidebar Sidebar Component Default</a>
+		</li>
+		<li class="p-1">
+			<a href="svelte-sidebar-single2">Svelte-sidebar Sidebar Component Styled</a>
+		</li>
+		<li class="p-1">
+			<a href="svelte-sidebar-multiple">Svelte-sidebar Multiple-menu Sidebar Default</a>
+		</li>
+		<li class="p-1">
+			<a href="svelte-sidebar-multiple2">Svelte-sidebar Multiple-menu Sidebar Default</a>
+		</li>
+	</ul>
+
+	<h2 class="p-1">Modal</h2>
+	<ul class="list-inside">
+		<li class="p-1"><a href="modals/example1">Simple</a></li>
+		<li class="p-1"><a href="modals/example2">Actions</a></li>
+		<li class="p-1"><a href="modals/example3">Mulitple</a></li>
+		<li class="p-1"><a href="modals/example4">Sign In</a></li>
+	</ul>
+</div>
