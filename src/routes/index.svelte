cc2e81b7f47520674d7865f91f11571adc4fed6a
Shinichi Okada <okada.shin@gmail.com>
2022-01-30 18:22:44 +0900

    Moved svelte-flow to the own repo.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,9 @@
-<div class="p-8 container mx-auto md:container md:mx-auto">
-	<h1 class="p-1">CodewithShin: SvelteKit Demo</h1>
-	<h2 class="p-1">Sidebar</h2>
-	<ul class="list-inside">
-		<li class="p-1">
-			<a href="svelte-sidebar-single">Svelte-sidebar Sidebar Component Default</a>
-		</li>
-		<li class="p-1">
-			<a href="svelte-sidebar-single2">Svelte-sidebar Sidebar Component Styled</a>
-		</li>
-		<li class="p-1">
-			<a href="svelte-sidebar-multiple">Svelte-sidebar Multiple-menu Sidebar Default</a>
-		</li>
-		<li class="p-1">
-			<a href="svelte-sidebar-multiple2">Svelte-sidebar Multiple-menu Sidebar Default</a>
-		</li>
-	</ul>
+<script>
+</script>
 
-	<h2 class="p-1">Modal</h2>
-	<ul class="list-inside">
-		<li class="p-1"><a href="modals/example1">Simple</a></li>
-		<li class="p-1"><a href="modals/example2">Actions</a></li>
-		<li class="p-1"><a href="modals/example3">Mulitple</a></li>
-		<li class="p-1"><a href="modals/example4">Sign In</a></li>
-	</ul>
-</div>
+<ul>
+	<li><a href="/alert">Alerts</a></li>
+	<li><a href="/cards">Cards</a></li>
+	<li><a href="/modals">Modals</a></li>
+	<li><a href="/tabs">Tabs</a></li>
+</ul>
--- src/routes/modals/__layout.svelte
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.min.css" />
-
-<slot />
